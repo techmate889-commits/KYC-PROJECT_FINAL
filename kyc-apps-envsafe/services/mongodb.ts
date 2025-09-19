1662a84c1a915cde05bb30be7434bd624f7e656d
techmate889-commits <techmate889@gmail.com>
2025-09-19 00:23:08 -0600

    Create mongodb.ts

---
+++ kyc-apps-envsafe/services/mongodb.ts
@@ -0,0 +1,25 @@
+import { MongoClient } from "mongodb";
+
+const uri = process.env.MONGODB_URI as string;
+
+if (!uri) {
+  throw new Error("Please add your MongoDB URI in Vercel environment variables");
+}
+
+const options = {};
+
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
+
+if (process.env.NODE_ENV === "development") {
+  if (!(global as any)._mongoClientPromise) {
+    client = new MongoClient(uri, options);
+    (global as any)._mongoClientPromise = client.connect();
+  }
+  clientPromise = (global as any)._mongoClientPromise;
+} else {
+  client = new MongoClient(uri, options);
+  clientPromise = client.connect();
+}
+
+export default clientPromise;
